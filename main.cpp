717c4e491d1add61ba41df07fe2da421c7dcd497
KATANADIE <larichev.po@students.sch2.ru>
2021-09-24 20:32:46 +0300

    Added a function to change questions in file

--- main.cpp
+++ main.cpp
@@ -1,91 +1,24 @@
 #include <iostream>
 #include <fstream>
-<<<<<<< HEAD
-
-void putOut(string x)
-{
-    cout << x << endl;
-    =======
-    map<string, int> books;
-    int total = 0;
-    void putOut(string x)
-    {
-        cout << x << endl;
-        int pos = x.rfind(" ");
-        string temp = x.substr(pos + 1, x.size() - pos - 1);
-        total = total + 1 -  min(books[temp], 1);
-        books[temp]++;
-    }
-
-    void printOutAnswers(int numberOfField) {
-        total = 0;
-        ifstream fin("answers.txt");
-        string s;
-        vector <string> names;
-        names.clear();
-        if (numberOfField == 1) {
-            while (fin >> s) {
-                {
-                    names.push_back(s);
-                }
-                sort(names.begin(), names.end());
-            }
+void changeQuestions() {
+    cout << "If you want to change the quesions, enter 1. Else enter no, and the program will terminate." << endl;
+    int val = 0;
+    cin >> val;
+    if(val == 1) {
+        cout << "Ok, opening questions file";
+        ofstream fout("questions.txt");
+        cout << "Enter the number of questions you want the file to have" << endl;
+        ofstream cleared("answers.txt");
+        cleared << endl;
+        cleared.close();
+        int numOfQuestions = 0;
+        cin >> numOfQuestions;
+        for(int i = 0; i < numOfQuestions; ++i) {
+            string s1;
+            cin >> s1;
+            fout << s1 << endl;
         }
-
-        if (numberOfField == 2) {
-            while(fin >> s)
-            {
-                int pos = s.find(" ");
-                string temp = s.substr(pos, s.rfind(" "));
-                names.push_back(s);
-            }
-            sort(names.begin(), names.end());
-        }
-
-        if (numberOfField == 3) {
-            while(fin >> s)
-            {
-                int pos = s.rfind(" ");
-                string temp = s.substr(pos, s.size() - pos);
-                names.push_back(s);
-            }
-            sort(names.begin(), names.end());
-        }
-
-
+    } else {
+        return;
     }
-    for(auto x : names)
-    {
-        putOut(x);
-    }
-
-    cout << "Amount of people who passed the survey - " << names.size() << endl;
-    cout << "Amount of books present - " << total;
-    fin.close();
-}
-
-void checkQuestions()
-{
-    ifstream fin("questions.txt");
-    ofstream fout("answers.txt");
-    string s;
-    string answer;
-    for(int i = 0; i < 3; ++i)
-    {
-        fin >> s;
-        cout << s << endl;
-        cin >> answer;
-        fout << answer << " ";
-    }
-    fout << endl;
-    fin.close();
-    fout.close();
-}
-
-signed main()
-{
-    string s;
-    cin >> s;
-    if(s == "OPROS") checkQuestions();
-    else printOutAnswers();
 }
